44cc11fb7bd23e9df6ad7b1c4978d380ec3ea6ef
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 15:17:53 +0100

    Added default and parameterised constructor to Dot class.

--- java/src/ie/tudublin/Dot.java
+++ java/src/ie/tudublin/Dot.java
@@ -7,6 +7,15 @@ public class Dot implements Renderable {
     private PVector pos;
     private float w, h;
 
+    public Dot(UI ui) {
+        this(ui, 10, 10);
+    }
+
+    public Dot(UI ui, float x, float y) {
+        this.ui = ui;
+        pos = new PVector(x, y);
+    }
+
     @Override
     public void update() {
 
