251000a7fadb4b0ca1666b3bfb836375ba8f34f6
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 14:44:41 +0100

    Implemented Render method.

--- java/src/ie/tudublin/DotGrid.java
+++ java/src/ie/tudublin/DotGrid.java
@@ -14,7 +14,12 @@ public class DotGrid implements Renderable {
 
     @Override
     public void render() {
-
+        ui.fill(0, 0, 255);
+        for(int i = 10; i < ui.width; i += 10) {
+            for(int j = 10; j < ui.height; j += 10) {
+                ui.ellipse(i, j, 3, 3);
+            }
+        }
     }
     
 }
