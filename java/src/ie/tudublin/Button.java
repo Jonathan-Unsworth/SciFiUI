b2ce49bbd9666f021f73aeac323fe5cb05ff2c7e
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 14:38:08 +0100

    Added text field to the button class.

--- java/src/ie/tudublin/Button.java
+++ java/src/ie/tudublin/Button.java
@@ -6,16 +6,18 @@ public class Button implements Renderable, Clickable {
     private UI ui;
     private PVector pos;
     private float w, h;
+    private String text;
 
     public Button(UI ui) {
-        this(ui, ui.width / 2, ui.height / 2, 100, 100);
+        this(ui, ui.width / 2, ui.height / 2, 100, 100, "Default");
     }
 
-    public Button(UI ui, float x, float y, float w, float h) {
+    public Button(UI ui, float x, float y, float w, float h, String text) {
         this.ui = ui;
         pos = new PVector(x, y);
         this.w = w;
         this.h = h;
+        this.text = text;
     }
 
     @Override
@@ -37,6 +39,9 @@ public class Button implements Renderable, Clickable {
     public void render() {
         ui.fill(0, 255, 0);
         ui.rect(pos.x, pos.y, w, h);
+        ui.fill(255);
+        ui.textAlign(ui.CENTER, ui.CENTER);
+        ui.text(text, pos.x + (w / 2), pos.y + (h / 2));
     }
     
 }
