e33cb01e17d40f8624a008619c7b1d68e9f74b05
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 14:23:56 +0100

    Added a list of renderable objects and added a render phase to the draw method.

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -1,25 +1,35 @@
 package ie.tudublin;
 
+import java.util.List;
+import java.util.ArrayList;
 import processing.core.PApplet;
 
 public class UI extends PApplet
 {
+    private List<Renderable> renderables;
+
     public void settings()
     {
-        size(800, 800);
+        size(600, 600);
         // Use fullscreen instead of size to make your interface fullscreen
         //fullScreen(P3D); 
     }
 
     public void setup()
     {
-        
+        renderables = new ArrayList<>();
+        renderables.add(new Button(this));
     }
 
     public void draw()
     {
         background(0);
         
+        // Render Phase
+        for(Renderable renderable : renderables) {
+            renderable.update();
+            renderable.render();
+        }
     }
 }
 
