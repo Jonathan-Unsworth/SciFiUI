52da31fd2c47a35fc91a5c15203d944329da8417
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 14:42:03 +0100

    Created dot grid class.

---
+++ java/src/ie/tudublin/DotGrid.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+public class DotGrid implements Renderable {
+    private UI ui;
+
+    public DotGrid(UI ui) {
+        this.ui = ui;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+    
+}
