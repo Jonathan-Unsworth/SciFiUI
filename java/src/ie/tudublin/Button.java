ac6cf758b01724ab7181e3ac19bd926e63387d73
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 22:23:22 +0100

    Implemented onClick and clicked.

--- java/src/ie/tudublin/Button.java
+++ java/src/ie/tudublin/Button.java
@@ -7,36 +7,42 @@ public class Button implements Renderable, Clickable {
     private PVector pos;
     private float w, h;
     private String text;
+    private Renderable renderable;
+    private boolean clicked = false;
 
     public Button(UI ui) {
-        this(ui, ui.width / 2, ui.height / 2, 100, 100, "Default");
+        this(ui, ui.width / 2, ui.height / 2, 100, 100, "Default", null);
     }
 
-    public Button(UI ui, float x, float y, float w, float h, String text) {
+    public Button(UI ui, float x, float y, float w, float h, String text, Renderable renderable) {
         this.ui = ui;
         pos = new PVector(x, y);
         this.w = w;
         this.h = h;
         this.text = text;
+        this.renderable = renderable;
     }
 
     @Override
     public boolean isClicked() {
         if(ui.mousePressed) {
-            return (ui.mouseX > pos.x && ui.mouseX < pos.x + w &&
-                    ui.mouseY > pos.y && ui.mouseY < pos.y + h);     
+            if(ui.mouseX > pos.x && ui.mouseX < pos.x + w &&
+               ui.mouseY > pos.y && ui.mouseY < pos.y + h) {
+                    clicked = !clicked;
+               }    
         }
-        return false;
+        return clicked;
     }
 
     @Override
     public void onClick() {
-
+        renderable.update();
+        renderable.render();
     }
 
     @Override
     public void update() {
-
+        
     }
 
     @Override
@@ -46,6 +52,10 @@ public class Button implements Renderable, Clickable {
         ui.fill(255);
         ui.textAlign(ui.CENTER, ui.CENTER);
         ui.text(text, pos.x + (w / 2), pos.y + (h / 2));
+
+        if(clicked) {
+            onClick();
+        }
     }
     
 }
