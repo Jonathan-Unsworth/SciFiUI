cfda44eab42bc0d5835f9516a05ead4984d2c00d
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 15:15:12 +0100

    Created class Dot.

---
+++ java/src/ie/tudublin/Dot.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+import processing.core.PVector;
+
+public class Dot implements Renderable {
+    private UI ui;
+    private PVector pos;
+    private float w, h;
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+}
