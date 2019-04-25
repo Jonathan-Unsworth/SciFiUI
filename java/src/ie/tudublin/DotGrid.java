e5a6c25941c7056ad3c0f1756943af05cf05c50d
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 15:37:03 +0100

    Changed implementation to use dot objects instead of primitive ellipses.

--- java/src/ie/tudublin/DotGrid.java
+++ java/src/ie/tudublin/DotGrid.java
@@ -1,28 +1,38 @@
 package ie.tudublin;
 
+import processing.core.PApplet;
 import processing.core.PVector;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DotGrid implements Renderable {
     private UI ui;
     private PVector pos;
+    private List<Dot> dots;
 
     public DotGrid(UI ui) {
         this.ui = ui;
         pos = new PVector();
+        dots = new ArrayList<>();
+
+        for(int i = 10; i < ui.width; i += 10) {
+            for(int j = 10; j < ui.height; j += 10) {
+                dots.add(new Dot(ui, i, j));
+            }
+        }
     }
 
     @Override
     public void update() {
-
+        for(Dot dot : dots) {
+            dot.update();
+        }
     }
 
     @Override
     public void render() {
-        ui.fill(255, 0, 0);
-        for(pos.x = 10; pos.x < ui.width; pos.x += 10) {
-            for(pos.y = 10; pos.y < ui.height; pos.y += 10) {
-                ui.ellipse(pos.x, pos.y, 2, 2);
-            }
+        for(Dot dot : dots) {
+            dot.render();
         }
     }
     
