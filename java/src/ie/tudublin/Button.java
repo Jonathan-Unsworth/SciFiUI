5eca74d98689fe7b233689143a29ae33b21685b5
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-25 14:10:56 +0100

    Created button class.

---
+++ java/src/ie/tudublin/Button.java
@@ -0,0 +1,31 @@
+package ie.tudublin;
+
+import processing.core.PVector;
+
+public class Button implements Renderable, Clickable {
+    private UI ui;
+    private PVector pos;
+    private float w, h;
+
+    @Override
+    public boolean isClicked() {
+        return false;
+    }
+
+    @Override
+    public void onClick() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+    
+}
+
