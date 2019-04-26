8c8171200ea8b7869bec289edd30492763de9551
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-26 21:37:16 +0100

    Removed redundant code.

--- java/src/ie/tudublin/DotGrid.java
+++ java/src/ie/tudublin/DotGrid.java
@@ -1,18 +1,14 @@
 package ie.tudublin;
 
-import processing.core.PApplet;
-import processing.core.PVector;
 import java.util.List;
 import java.util.ArrayList;
 
 public class DotGrid implements Renderable {
     private UI ui;
-    private PVector pos;
     private List<Dot> dots;
 
     public DotGrid(UI ui) {
         this.ui = ui;
-        pos = new PVector();
         dots = new ArrayList<>();
 
         for(int i = 10; i < ui.width; i += 10) {
