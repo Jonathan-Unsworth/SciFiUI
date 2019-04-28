70b4fc9c98076148211bf2e05e99f156035d466e
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-28 17:06:20 +0100

    Implemented render method.

--- java/src/ie/tudublin/Cursor.java
+++ java/src/ie/tudublin/Cursor.java
@@ -1,6 +1,16 @@
 package ie.tudublin;
 
+import processing.core.PApplet;
+
 public class Cursor implements Renderable {
+    private UI ui;
+    private float velocity = 0.01f; 
+    private float spin = 0; 
+    private float size = 100;
+
+    public Cursor(UI ui) {
+        this.ui = ui;
+    }
 
     @Override
     public void update() {
@@ -9,7 +19,15 @@ public class Cursor implements Renderable {
 
     @Override
     public void render() {
-
+        ui.fill(0, 255, 0);
+        spin += velocity; 
+        ui.pushMatrix(); 
+        ui.translate(ui.mouseX, ui.mouseY); 
+        ui.noStroke(); 
+        ui.fill(0, 255, 0); 
+        ui.rotate(spin); 
+        ui.triangle(30, 75, 58, 20, 86, 75);
+        ui.popMatrix(); 
     }
     
 }
