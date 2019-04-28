d7bf02a838460c77c46416bafe9a1904befccb6c
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-28 17:46:37 +0100

    Improved Cursor render method.

--- java/src/ie/tudublin/Cursor.java
+++ java/src/ie/tudublin/Cursor.java
@@ -1,13 +1,10 @@
 package ie.tudublin;
 
-import processing.core.PApplet;
-
 public class Cursor implements Renderable {
     private UI ui;
     private float velocity = 0.01f; 
     private float spin = 0; 
-    private float size = 100;
-
+    
     public Cursor(UI ui) {
         this.ui = ui;
     }
@@ -24,7 +21,7 @@ public class Cursor implements Renderable {
         ui.pushMatrix(); 
         ui.translate(ui.mouseX, ui.mouseY); 
         ui.noStroke(); 
-        ui.fill(0, 255, 0); 
+        ui.fill(0, 255, 255); 
         ui.rotate(spin); 
         ui.triangle(30, 75, 58, 20, 86, 75);
         ui.popMatrix(); 
