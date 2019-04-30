adbc1243a87c967fd486f4f7fad825fa32ba09a5
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-30 22:44:59 +0100

    Put comments in.

--- java/src/ie/tudublin/DotGrid.java
+++ java/src/ie/tudublin/DotGrid.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class DotGrid implements Renderable {
+    // Fields
     private UI ui;
     private List<Dot> dots;
 
+    // Constructor
     public DotGrid(UI ui) {
         this.ui = ui;
+
+        // Allocating memory for Dot ArrayList
         dots = new ArrayList<>();
 
+        // Adds Dots accross the screen
         for(int i = 10; i < ui.width; i += 10) {
             for(int j = 10; j < ui.height; j += 10) {
                 dots.add(new Dot(ui, i, j));
